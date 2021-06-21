aa63959c3b9e282f9c159600512e0ae8b51ea86f
RaquelMalo <maloraquel@hotmail.com>
2021-06-21 11:04:34 +0200

    Final (routing arreglado, añadido código definitivo)

--- proyecto/Angular/app/app.module.ts
+++ proyecto/Angular/app/app.module.ts
@@ -8,13 +8,14 @@ import { PieComponent } from './pie/pie.component';
 
 import { RouterModule, Routes } from '@angular/router';
 
+
 // Configuración de las rutas:
 // Es IMPORTANTE el orden en que se ponen las rutas. SE TIENE que poner LAS MÁS ESPECÍFICAS primero y luego las MÁS GENERALES
  // por que se ejecutan de arriba abajo
  const appRoutes: Routes = [
   // { path: 'masInfo', component: VideoComponent },
-  { path: '', component: GaleriaComponent }, // Sintaxis: { path: 'Nombre_Ruta', component: Nombre_Componente }
-  { path: '', redirectTo: '/', pathMatch: 'full' },  // Con REDIRECT para página de Inicio de la Aplicación
+  { path: 'inicio', component: GaleriaComponent }, // Sintaxis: { path: 'Nombre_Ruta', component: Nombre_Componente }
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },  // Con REDIRECT para página de Inicio de la Aplicación
 
 ];
 
@@ -27,7 +28,7 @@ import { RouterModule, Routes } from '@angular/router';
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, { useHash: true }) // Para que coja las rutas el servidor añadir useHash
   ],
   providers: [],
   bootstrap: [AppComponent]
